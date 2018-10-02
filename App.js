4903ba9c0f87ba0daf71131394175540fa2f2030
AWSYS-I\rafael.chua <rafael.chua@awsys-i.com>
2018-10-02 10:25:00 +0800

    trending added

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {StyleSheet, Dimensions, Text, View, FlatList, ImageBackground, ActivityIndicator, ScrollView, StatusBar} from 'react-native';
+import {StyleSheet, Dimensions, Text, View, FlatList, ImageBackground, ActivityIndicator, ScrollView, StatusBar, Image} from 'react-native';
 import * as Kitsu from './Kitsu'
 import LinearGradient from 'react-native-linear-gradient';
 
@@ -32,7 +32,7 @@ export default class App extends React.Component {
     render() {
         if(this.state.isLoading) {
             return(
-                <View style={styles.container}>
+                <View style={{flex: 1, backgroundColor: '#141414', justifyContent: 'center', alignItems: 'center'}}>
                     <ActivityIndicator size="large" color="#ccc" />
                 </View>
             );
@@ -41,18 +41,25 @@ export default class App extends React.Component {
             <ScrollView contentContainerStyle={styles.container}>
                 <StatusBar backgroundColor='transparent' barStyle='light-content' translucent={true}/>
                 <ImageBackground source={{uri: this.state.spotlight.attributes.posterImage.large}} style={styles.spotlight}>
-                    <LinearGradient colors={['transparent', 'rgba(14,14,14,0.65)', 'rgba(14,14,14,0.95)']} style={styles.overlay}>
+                    <LinearGradient colors={['transparent', 'rgba(20,20,20,.6)', 'rgb(20,20,20)']} style={styles.overlay}>
+                        <Text style={styles.spotlightTitle}>{this.state.spotlight.attributes.canonicalTitle}</Text>
                     </LinearGradient>
                 </ImageBackground>
-                <Text style={styles.header}>Trending Anime</Text>
-                <FlatList
-                    showsVerticalScrollIndicator={false}
-                    scrollEventThrottle={300}
-                    keyExtractor={(item, index) => item.id.toString()}
-                    data={this.state.trendingAnime}
-                    renderItem={({item, separators}) => (
-                        <Text>{item.id}</Text>
-                )}/>
+                <View style={styles.mainContainer}>
+                    <Text style={styles.header}>Trending Anime</Text>
+                    <FlatList
+                        style={{marginTop: 5}}
+                        horizontal={true}
+                        showsHorizontalScrollIndicator={false}
+                        scrollEventThrottle={300}
+                        keyExtractor={(item, index) => item.id.toString()}
+                        data={this.state.trendingAnime}
+                        renderItem={({item, separators}) => (
+                            <View style={styles.item}>
+                                <Image source={{uri: item.attributes.posterImage.small}} style={styles.image}/>
+                            </View>
+                        )}/>
+                </View>
             </ScrollView>
         );
     }
@@ -61,9 +68,11 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
         backgroundColor: '#141414',
+    },
+    mainContainer: {
+        padding: 20,
+        marginTop: -50,
     },
     welcome: {
         fontSize: 20,
@@ -72,18 +81,31 @@ const styles = StyleSheet.create({
     },
     spotlight: {
         width: windowWidth,
-        height: windowHeight - 100,
+        height: windowHeight - 200,
     },
     overlay: {
         position: 'absolute',
         bottom: 0,
         left: 0,
         width: windowWidth,
-        height: '25%'
+        height: '30%'
     },
     header: {
         color: 'white',
         fontWeight: '700',
-        fontSize: 14
+        fontSize: 15
+    }, 
+    item: {
+        marginRight: 10,
+    },
+    image: {
+        width: 125,
+        height: 170
+    },
+    spotlightTitle: {
+        fontSize: 24,
+        color: 'white',
+        justifyContent: 'center',
+        alignSelf: 'center'
     }
 });
